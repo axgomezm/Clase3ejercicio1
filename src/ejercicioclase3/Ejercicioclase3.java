d87f6bdf7fb5bd3fc3662ea5e457bb0ab14d3fc6
axgomezm <axgomezm@BE401S103C29ING.unal.loc>
2019-04-15 08:38:13 -0500

    Clase3: intento 2 porque soy un ijueputa manco para compilar

--- src/ejercicioclase3/Ejercicioclase3.java
+++ src/ejercicioclase3/Ejercicioclase3.java
@@ -3,14 +3,30 @@ package ejercicioclase3;
 public class Ejercicioclase3 {
     
     public static void main(String[] args) {
+        boolean resultado;
         Banco p1=new Banco(1012,1234,"Magdalena sanchez",0);
-        p1.consignar(100000);
-        System.out.println("Su nuevo saldo es: "+p1.getSaldo());
+        resultado=p1.consignar(100000);
+        if(resultado==true)
+        {
+            System.out.println("consignacion exitosa:)");
+        }
+        else{
+        System.out.println("consignacion denegada:(");
+        }
+        
+        System.out.println(p1.getPersona()+" su nuevo saldo es: "+p1.getSaldo());
         Banco p2=new Banco(1013,9876,"Joselito Perez",500000);
-        p2.cambiar(1225);
+        resultado=p2.cambiar(1228);
+        if(resultado==true)
+        {
+            System.out.println("Cambio de clave exitoso:)");
+        }
+        else{
+        System.out.println("Cambio de denegado :(");
+        }
         p2.consultar();
-        System.out.println("Su saldo es: "+p2.consultar());
-        System.out.println(p2.getClave());
+        System.out.println(p2.getPersona()+" su saldo es: "+p2.consultar());
+        System.out.println(p2.getPersona()+" su nueva clave es: "+p2.getClave());
     }
     
     
